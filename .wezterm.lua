18c046f1be0c0541324569c29ff32dea7c5e48a7
kina <jam006097@gmail.com>
2026-04-29 15:18:19 +0900

    add: neovim Lazyvim config

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Font
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'JetBrainsMono Nerd Font' 
 config.font_size = 14.0
 
 -- Appearance
